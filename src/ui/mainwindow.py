3b69dca3b68850247079e1b45bc99ed5ac001999
tutel <john.g.zioupos@gmail.com>
2026-02-06 14:24:32 +0200

    prevent multiple plot displays
    Added a messagebox warning for repeated plot calls and a global flag to track plot display status.

--- src/ui/mainwindow.py
+++ src/ui/mainwindow.py
@@ -1,7 +1,10 @@
 import tkinter as tk
+from tkinter import messagebox
 from .matplotlib import display_plot
 
+plot_dis = False
 def startui(compute_callback):
+    global plot_dis
     root = tk.Tk()
     root.title("My Math UI")
 
@@ -11,10 +14,13 @@ def startui(compute_callback):
     tk.Checkbutton(root, text="Show plot", variable=state).pack()
 
     def on_click():
-        if state.get():
+        global plot_dis
+        if state.get() and plot_dis == False:
             x, y, dy, roots = compute_callback
             display_plot(root, x, y, dy, roots)
-
+            plot_dis = True
+        else:
+            messagebox.showerror("Warning", "The plot function was called and returned a plot")
     tk.Button(root, text="Ready", command=on_click).pack()
 
     root.mainloop()
