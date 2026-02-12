7d110967c322849964e6ac6b1ee796b9d40f9037
John Zioupos <john.g.zioupos@gmail.com>
2026-02-12 16:54:31 +0200

    added asteval logic to help with user inputed funcs

--- src/main.py
+++ src/main.py
@@ -1,14 +1,27 @@
-import math
-import matplotlib.pyplot as plt
+import numpy
 import engine as engine
 from ui.mainwindow import startui
+from asteval import Interpreter
 
-def f(x): 
-    d = math.radians(x)
-    return (math.sin(d)*math.cos(d*d)) * 0.45
-#def f(x):
- #   return x*x - x*x*x
 
+#def f(x): 
+#    d = math.radians(x)
+#    return (math.sin(d)*math.cos(d*d)) * 0.45
+
+aeval = Interpreter(use_numpy=True)
+aeval.symtable['np'] = numpy
+
+function_raw = input("Enter f(x) function: ").replace("^", "**")
+
+func_def = f"def f(x): return {function_raw}"
+aeval(func_def)
+
+f = aeval.symtable.get("f")
+
+if f:
+    print("Function was Created succesfully")
+else:
+    print("Error while creating function")
 
 def compute():
     x_values, y_values = engine.sample(f,1000,-360,360)
